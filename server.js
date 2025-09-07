56b824f55b47cae5cdabec9d354512d4dc520d22
ahmad-rk02 <razakhanahmad68@gmail.com>
2025-09-07 19:20:05 +0530

    added

--- server.js
+++ server.js
@@ -23,11 +23,12 @@ const allowedOrigins = [
 
 
 app.use(cors({
-  origin: ["http://localhost:5173", "https://your-frontend.vercel.app"],
+  origin: "http://localhost:5173",
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true
 }));
+
 
 app.use(express.json());
 
