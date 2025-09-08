921c606ac5c67d97f2add9c1a8fa55b0c2516256
ahmad-rk02 <razakhanahmad68@gmail.com>
2025-09-08 13:10:14 +0530

    added

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: ["http://localhost:5173"],
+  origin: ["http://localhost:5173","https://ahmad-raza-khan-portfolio.netlify.app/"],
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true
